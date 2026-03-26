3576517a1305fc365b25334edfec8b46124e041b
Chin Mun Yau <munyauchim@gmail.com>
2026-03-26 17:39:01 +0800

    implemented Board.java 	Will need to confirm which class takes ownership of the noble cards

---
+++ src/splendor/model/Board.java
@@ -0,0 +1,131 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import splendor.config.GameConfig;
+
+public class Board {
+    private Map(GemType, Integer) gemBank;
+    private Card[][] visibleCards; // 3x4 array of face up cards
+    private Deck[] decks; // have 3 levels 0, 1, 2
+    private List<Noble> nobles;
+    
+    public Board(GameConfig gameConfig, List<Card> allCards, List<Noble> nobles, int numPlayers) {
+        // initialise gem bank
+        for (GemType gemType : GemType.values()) {
+            if (gemType == GemType.GOLD) {
+                gemBank.put(gemType, gameConfig.getGoldCount());
+            } else {
+                gemBank.put(gemType, getGemCount(numPlayers))
+            }
+        }
+
+        // card sorting
+        List<Card> level1Cards = new ArrayList<>();
+        List<Card> level2Cards = new ArrayList<>();
+        List<Card> level3Cards = new ArrayList<>();
+
+        for (Card card : allCards) {
+            int level = card.getLevel();
+            switch (level) {
+                case 1:
+                    level1Cards.add(card);
+                    break;
+                case 2:
+                    level2Cards.add(card);
+                    break;
+                case 3:
+                    level3Cards.add(card);
+                    break;
+                default:
+                    break;
+            }
+        }
+        Deck[] decks = {new Deck(level1Cards), new Deck(level2Cards), new Deck(level3Cards)};
+
+        // shuffle deck
+        for (int i = 0; i < 3; i++) {
+            decks[i].shuffle();
+        }
+
+        // Deal Cards
+        visibleCards = new Card[3][4];
+        for (int i = 0; i < visibleCards.length; i++) {
+            for (int j = 0; j < visibleCards.length; j++) {
+                visibleCards[i][j] = deck[i].deal();
+            }
+        }
+
+        // Reveal Noble cards
+        List<Noble> shuffled = nobles;
+        Collections.shuffle(shuffled);
+        this.nobles = shuffled;
+    }
+
+    public refillCards() {
+        for (int level = 0; level < 3; level++) {
+            for (int slot = 0; slot < 4; slot++) {
+                if (visibleCards[level][slot] == null) {
+                    visibleCards[level][slot] = decks[level].deal();
+                }
+            }
+        }
+    }
+
+    public Map(GemType, Integer) getGemBank() {
+        return gemBank;
+    }
+
+    public Card[][] getVisibleCards() {
+        return visibleCards;
+    }
+
+    public List<Noble> getNobles() {
+        return nobles;
+    }
+
+    public void takeGems(Map<GemType, Integer> mp) {
+        for (Map.Entry<GemType, Integer> entry : mp.entrySet()) {
+            GemType type = entry.getKey();
+            int count = entry.getValue();
+            if (count != 0) {
+                gemBank.put(type, gemBank.get(type) - count);
+            }
+        }
+    }
+
+    public void returnGems(Map<GemType, Integer> mp) {
+        for (Map.Entry<GemType, Integer> e : mp.entrySet()) {
+            // assumption that the gemBank will contain all the gem (keys)
+            Gemtype g = e.getKey();
+            Integer i = e.getValue();
+            gemBank.put(g, i + gemBank.get(g));
+        }
+    }
+
+    public Card takeVisibleCard(int level, int slot) {
+        Card cardTaken = visibleCards[level][slot];
+        visibleCards[level][slot] = decks[level].deal();
+        return Card;
+    }
+
+    public card takeReserveCard (int level) {
+        return deck[level].deal();
+    }
+
+    public void refillCards() {
+        for (int i = 0; i < card.length; i++) {
+            for (int i = j;  j < card[0].length; j++) {
+                if (card[i][j] == null) {
+                    card[i][j] = decks[i].deal();
+                }
+            }
+        }
+    }
+
+    public void removeNoble(Noble noble) {
+        nobles.remove(noble);
+    }
+}

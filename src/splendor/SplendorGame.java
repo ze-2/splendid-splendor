692d133bfc75643eef5258dbec8e64265dfbef67
zayna31 <zayna.ng.2025@computing.smu.edu.sg>
2026-03-29 10:33:56 +0800

    fix player initialization and test main

--- src/splendor/SplendorGame.java
+++ src/splendor/SplendorGame.java
@@ -25,15 +25,18 @@ public class SplendorGame {
         String type;
         for (int i = 0; i < numOfPlayers; i++) {
             name = ui.promptPlayerName(i + 1);
-            type = ui.promptPlayerType(i + 1); 
+            type = ui.promptPlayerType(i + 1).trim().toLowerCase(); 
 
             Player p;
 
             if (type.equals("human")) {
-                p = new HumanPlayer(name);
+                p = new HumanPlayer(name, ui);
             }
             else if (type.equals("ai")) {
                 p = new AIPlayer(name);
+            }
+            else {
+                throw new IllegalArgumentException("Invalid player type: " + type);
             }
             // create player list
             players.add(p);
@@ -53,8 +56,14 @@ public class SplendorGame {
         // build board 
         Board board = new Board(config, cards, nobles, numOfPlayers);
 
-        GameEngine engine = new GameEngine(players, board, ui, config);
+        WinChecker winChecker = new WinChecker();
+        ActionValidator validator = new ActionValidator();
+
+        GameEngine engine = new GameEngine(players, board, ui, config, winChecker, validator);
         engine.start();
 
     }
+
+   
+    
 }
